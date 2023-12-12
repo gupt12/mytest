b50f2c252285e67a950a5dac60d88041187ac0ac
gupt12 <45801913+gupt12@users.noreply.github.com>
2023-12-12 06:58:49 +0000

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -5,6 +5,6 @@ resource "oci_identity_compartment" "vcndv-compartment" {
     # Required
     compartment_id = var.compartment_id
     description = "Compartment for DEV PVT Endpoint resources"
-    name = "ESDD_PVTEND_DEV"
+    name = "ESDD_PVTEND_DEV "
     enable_delete = true
 }
